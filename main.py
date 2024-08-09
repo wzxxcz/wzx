fab28097df71853b64c42ede48f3fcc0729a59fa
wzxxcz <155713461+wzxxcz@users.noreply.github.com>
2024-08-09 09:03:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from datetime import datetime
 # 执行开始时间
 timestart = datetime.now()
 # 报时
-print(f"time: {datetime.now().strftime("%Y%m%d_%H_%M_%S")}")
+import datetime
+
+print(f"time: {datetime.datetime.now().strftime('%Y%m%d_%H_%M_%S')}")
 # 定义要访问的多个URL
 urls = [
     'https://raw.githubusercontent.com/iptv-org/iptv/master/streams/cn.m3u',
